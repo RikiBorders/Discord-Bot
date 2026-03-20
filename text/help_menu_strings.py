b9d0f33016122f7aa54d822cbfeef70e6faebaf3
RikiBorders <ninjab02@hotmail.com>
2026-03-20 07:57:40 +0000

    Update help menu with hardcoded list of commands

--- text/help_menu_strings.py
+++ text/help_menu_strings.py
@@ -1,7 +1,10 @@
 from constant.Constants import BOT_NAME
 
 HELP_MENU_DESCRIPTION = f"Welcome to the {BOT_NAME} Help Menu! \n\n" \
-    "Below you'll find a comprehensive list of supported commands."
+    "Below you'll find a comprehensive list of supported commands.\n"\
+    "1. **Help**: Displays this help menu.\n"\
+    "2. **ShareRules**: (Admin Only) Shares the server rules in a specified channel.\n"\
+    "3. **Announce**: (Admin Only) Sends an announcement to the announcement channel.\n"\
 
 HELP_MENU_COMMANDS = {
     "Help": "Displays this help menu"
